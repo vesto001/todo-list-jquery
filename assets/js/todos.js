884966e981d0dbbd54a087a3466d876ead0e08ad
vesto001 <vicestopinan@yahoo.com>
2018-03-05 14:11:19 -0500

    Migrate project

---
+++ assets/js/todos.js
@@ -0,0 +1,18 @@
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("selected");
+});
+
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress( function(event) {
+  if (event.which === 13) {
+    const todoText = $(this).val();
+    $(this).val("");
+    $("ul").append(`<li><span><i class="fa fa-trash"></i></span> ${todoText}</li>`)
+  }
+});
